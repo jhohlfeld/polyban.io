2e3365c9d6c006b45a08829869adf7324c23af7a
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2015-02-28 17:29:30 +0100

    adjusting css breakpoints

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,22 +4,12 @@
   // jshint devel:true
   console.log('\'Allo \'Allo!');
 
-  // $('.ui.sticky')
-  //   .sticky();
-
-  // var sections = {
-  //   'we-fix-your-mojo': 'one',
-  //   'how-we-do-it': 'three',
-  //   'about-us': 'four'
-  // };
-
   var Router = Backbone.Router.extend({
     routes: {
       ':section': 'navigate'
     },
 
     navigate: function(section) {
-      // var target = $('#' + sections[section]);
       var target = $('a[name="#/' + section + '"]');
       if (target.length) {
         $('html,body').animate({
@@ -32,4 +22,6 @@
   new Router();
 
   Backbone.history.start();
+
+  $('.ui.dropdown').dropdown();
 })();
