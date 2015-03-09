7e363f947a404ed0b7f7235195f41a6e8ab0ecfd
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2015-03-09 15:33:21 +0100

    Fixing css (footer); Setting up a mailchimp contact form; optimizing images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,83 @@
 (function() {
   'use strict';
 
+  // set up other ui elements
+
+  var contactForm = $('.ui.modal.form');
+  contactForm.modal();
+  contactForm.form({
+    email: {
+      identifier: 'EMAIL',
+      rules: [{
+        type: 'email',
+        prompt: 'Please enter your email address'
+      }]
+    },
+    firstname: {
+      identifier: 'FIRSTNAME',
+      rules: [{
+        type: 'empty',
+        prompt: 'Please enter your first name'
+      }]
+    },
+    lastname: {
+      identifier: 'LASTNAME',
+      rules: [{
+        type: 'empty',
+        prompt: 'Please enter your last name'
+      }]
+    }
+  }, {
+    on: 'submit'
+  });
+
+  // register contact form buttons
+
+  $('.contact').on('click', function(e) {
+    contactForm.modal('show');
+  });
+
+  var actionForm = $('.ui.action.input');
+  actionForm.form({
+    email: {
+      identifier: 'email',
+      rules: [{
+        type: 'email',
+        prompt: 'Please give a valid email address'
+      }]
+    }
+  }, {
+    on: 'submit',
+    onSuccess: function(e) {
+      contactForm.form('clear').form('set value', 'EMAIL', actionForm.form('get value', 'email')).modal('show');
+    }
+  });
+
+  actionForm.on('submit', function(e) {
+    e.preventDefault();
+  });
+
+  // email obfuscator
+
+  function unCryptMailto(s) {
+    var n = 0;
+    var r = '';
+    for (var i = 0; i < s.length; i++) {
+      n = s.charCodeAt(i);
+      if (n >= 8364) {
+        n = 128;
+      }
+      r += String.fromCharCode(n - 1);
+    }
+    return r;
+  }
+
+  function linkToUnCryptMailto(s) {
+    location.href = unCryptMailto(s);
+  }
+
+  // set up routing
+
   var Router = Backbone.Router.extend({
     routes: {
       ':section': 'navigate'
